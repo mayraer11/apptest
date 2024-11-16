45a6e95e61d04bc13584a2cf8d71bd4e6ad21773
mayraer11 <mayraer11@gmail.com>
2024-11-16 16:48:22 -0500

    redondeo

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -20,10 +20,10 @@ const tipocambio = ref(0)
   <p>Precio de Etiqueta:</p>
 	<input v-model="precio" placeholder="P.etiqueta" />
   <p>Calculos:</p>
-  <p>Tax 7%: {{ precio*0.07 }}</p>
+  <p>Tax 7%: {{ (precio*0.07).toFixed(2) }}</p>
   <p>Peso: {{ peso }}</p>
-  <p>Ganancia: {{ (parseFloat(precio)+ parseFloat(peso)+parseFloat(precio*0.07))*0.25 }}</p>
-  <p>Total: {{ parseFloat((parseFloat(precio)+ parseFloat(peso)+parseFloat(precio*0.07))*0.25) + parseFloat(precio) + parseFloat(precio*0.07) + parseFloat(peso) }}</p>
-  <p>En Soles: {{ (parseFloat((parseFloat(precio)+ parseFloat(peso)+parseFloat(precio*0.07))*0.25) + parseFloat(precio) + parseFloat(precio*0.07) + parseFloat(peso))*tipocambio }} </p>
+  <p>Ganancia: {{ ((parseFloat(precio)+ parseFloat(peso)+parseFloat(precio*0.07))*0.25).toFixed(2) }}</p>
+  <p>Total: {{ (parseFloat((parseFloat(precio)+ parseFloat(peso)+parseFloat(precio*0.07))*0.25) + parseFloat(precio) + parseFloat(precio*0.07) + parseFloat(peso)).toFixed(2) }}</p>
+  <p>En Soles: {{ ((parseFloat((parseFloat(precio)+ parseFloat(peso)+parseFloat(precio*0.07))*0.25) + parseFloat(precio) + parseFloat(precio*0.07) + parseFloat(peso))*tipocambio).toFixed(2) }} </p>
 
 </template>
