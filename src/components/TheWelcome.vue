74c174c7df7cb229c7c15979f917d9e84115416d
mayraer11 <mayraer11@gmail.com>
2024-11-16 15:30:41 -0500

    pc

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+
+const precio = ref(0)
+const peso = ref(0)
+const tipocambio = ref(0)
+
+</script>
+
+<template>
+  <p>Peso:</p>
+  <select v-model="peso">
+    <option disabled value="">0</option>
+    <option>5</option>
+    <option>10</option>
+    <option>15</option>
+  </select>
+    <p>Tipo de Cambio:</p>
+	<input v-model="tipocambio" placeholder="Tipo de cambio" />
+  <p>Precio de Etiqueta:</p>
+	<input v-model="precio" placeholder="P.etiqueta" />
+  <p>Calculos:</p>
+  <p>Tax 7%: {{ precio*0.07 }}</p>
+  <p>Peso: {{ peso }}</p>
+  <p>Ganancia: {{ (parseFloat(precio)+ parseFloat(peso)+parseFloat(precio*0.07))*0.25 }}</p>
+  <p>Total: {{ parseFloat((parseFloat(precio)+ parseFloat(peso)+parseFloat(precio*0.07))*0.25) + parseFloat(precio) + parseFloat(precio*0.07) + parseFloat(peso) }}</p>
+  <p>En Soles: {{ (parseFloat((parseFloat(precio)+ parseFloat(peso)+parseFloat(precio*0.07))*0.25) + parseFloat(precio) + parseFloat(precio*0.07) + parseFloat(peso))*tipocambio }} </p>
+
+</template>
